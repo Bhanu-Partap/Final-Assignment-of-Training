addff0c7d6e77fb83a6726f0a3c784e611314d64
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-13 11:58:02 +0530

    test case

--- test/testcase.js
+++ test/testcase.js
@@ -5,7 +5,7 @@ contract("final assignment test cases", async () => {
   it("should it setting the right value", async () => {
     const contr = await assignment.deployed();
 
-    const value = await contr.staking(100, "fixed", 100, true);
+    const value = await contr.staking(100, "fixed", 100, true,{from:"0xd9145CCE52D386f254917e481eB44e9943F39138", gas:300000});
     console.log(value);
     expect(await value.toNumber()).to.equal(100, "fixed", 100, true);
   });
