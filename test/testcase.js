946109f7cb3ddc26a4c5313d86f3f52cb0a5119d
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-13 17:16:30 +0530

    test case

--- test/testcase.js
+++ test/testcase.js
@@ -10,33 +10,28 @@ contract("final assignment test cases", (accounts) => {
     erC20 = await erc20.new();
     stakingcontract = await assignment.new(erC20.address);
   });
-  //ERC-20 approve function call
+  //ERC-20 approve allowance function call
 
   it("approving the contract", async () => {
     let delegate_address = accounts[1];
     let _amount = 100;
+    let Owner= accounts[0];
     const value = await erC20.approve(delegate_address, _amount, {
-      from: accounts[0],
+      from: accounts[0]
     });
     console.log(value);
-    var A = await value;
-    var B =await A.spender;
-    console.log(B)
-
-    expect( B).to.equal(
-      "0x6e402b78F98CF9bfb32757dd0EFc50fA056778bc"
+    const allow = await erC20.allowance(Owner,delegate_address);
+    console.log(allow);
+    expect( await value).to.equal( allow.toNumber()
     );
   });
 
-//   it("should it setting the right value", async () => {
-//     //   const ERC20 = await assignment.deployed();
-//     // const contractAssign = await assignment.deployed(ERC20);
+  it("should it setting the right value", async () => {
 
-//     const value = await stakingcontract.staking(100, "fixed", 100, true, {
-//       from: accounts[0],
-//       gas: 300000,
-//     });
-//     console.log(value);
-//     expect(await value.toNumber()).to.equal(100, "fixed", 100, true);
-//   });
+    const value = await stakingcontract.staking(100, "fixed", 100, true, {
+      from: accounts[0]
+    });
+    console.log(value);
+    expect(await value.toNumber()).to.equal(100, "fixed", 100, true);
+  });
 });
