f6f6f10c08f1dd7ce9dcd1716c383b479ac77617
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-13 11:40:34 +0530

    test case

--- test/testcase.js
+++ test/testcase.js
@@ -1,13 +1,12 @@
 const assignment = artifacts.require("final_assignment_staking_1");
+const { expect } = require('chai');
 
-contract("final assignment test cases", async ()=>{
-    beforeEach(async()=>{
-        const contr = await assignment.deployed();  
-    })
-    it("should it setting the right value", async()=>{
-        const value = await contr.staking.call(100,"fixed",100,true);
-        console.log(value);
-        expect(await value.toNumber().to.equal(100,"fixed",100,true));
+contract("final assignment test cases", async () => {
+  it("should it setting the right value", async () => {
+    const contr = await assignment.deployed();
 
-    })
-})
+    const value = await contr.staking(100, "fixed", 100, true);
+    console.log(value);
+    expect(await value.toNumber()).to.equal(100, "fixed", 100, true);
+  });
+});
