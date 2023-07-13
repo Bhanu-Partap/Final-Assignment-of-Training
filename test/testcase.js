f2b25199a15504ed6c1e59966a9fcca36e7c3d90
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-13 11:11:07 +0530

    test case

--- test/testcase.js
+++ test/testcase.js
@@ -5,6 +5,9 @@ contract("final assignment test cases", async ()=>{
         const contr = await assignment.deployed();  
     })
     it("should it setting the right value", async()=>{
-        
+        const value = await contr.staking.call(100,"fixed",100,true);
+        console.log(value);
+        expect(await value.toNumber().to.equal(100,"fixed",100,true));
+
     })
 })
